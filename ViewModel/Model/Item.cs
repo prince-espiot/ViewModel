f7410c10416de7eeb5cf835a3b250e69ca1c6ffb
Prince Okumo <prokum@ttu.ee>
2024-09-27 14:07:21 +0300

    ViewModel implemeneted

---
+++ ViewModel/Model/Item.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ViewModel.Model
+{
+    internal class Item
+    {
+        public string? Name { get; set; }
+        public string? SerialNumber { get; set; } 
+        public string? Quantity { get; set; }
+    }
+}
---
+++ ViewModel/ViewModel/MainWindowViewModal.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ViewModel.Model;
+using ViewModel.MVVM;
+namespace ViewModel.ViewModel
+{
+    internal class MainWindowViewModal:ViewModalBase
+
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public MainWindowViewModal()
+        {
+            Items = new ObservableCollection<Item>();
+            Items.Add(new Item
+            {
+                Name = " Prince",
+                SerialNumber = "00041",
+                Quantity = "5"
+            });
+            Items.Add(new Item
+            {
+                Name = "Prosper",
+                SerialNumber = "10545",
+                Quantity = "8"
+            });
+        }
+        private Item selectedItem;
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set {
+                selectedItem = value;
+                OnPropertyChanged();
+            
+            }
+        }
+
+       
+
+    }
+}
